1f5006daa539d3a2a5ea56b2551ccc5e56fff398
marjutubli@gmail.com <marjutubli@gmail.com>
2018-04-19 05:23:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const theBeatlesPlay = (musicians, instruments) => {
   var arr = [];
   
   for(let i = 0; i<musicians.length; i++){
-      arr.push(musicians[i] + ' plays ' + instruments[i])
+      arr.push(musicians[i] + ' plays ' + instruments[i]);
   }
-  return arr
+  return arr;
 };
