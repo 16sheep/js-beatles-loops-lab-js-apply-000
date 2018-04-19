33ea26ff6c91e42e60a0c8fd800b803ea7829fec
marjutubli@gmail.com <marjutubli@gmail.com>
2018-04-19 05:31:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,3 +20,15 @@ const johnLennonFacts = (facts) => {
   return arr;
 };
 
+const johnLennonFacts = (facts) => {
+  let arr = [];
+  let i = 0;
+  
+  while(i<facts.length){
+    arr.push(facts[i] + '!!!');
+    i++;
+    
+  }
+  return arr;
+};
+
